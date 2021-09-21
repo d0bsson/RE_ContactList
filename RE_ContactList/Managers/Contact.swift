b338c86df734e363a160141aa5714a6d77002c9a
David <ber.dev@yandex.ru>
2021-09-21 14:30:42 +0300

    create model

--- RE_ContactList/Managers/Contact.swift
+++ RE_ContactList/Managers/Contact.swift
@@ -10,7 +10,7 @@ struct Contact {
     static let shared = Contact()
     
     let names = ["Jeremy", "Steven", "Bings", "Obansky", "Bakkery"]
-    let seconNames = ["Balansk", "Finansk", "Collabnsk", "Obninsk", "Kertansk"]
+    let secondNames = ["Balansk", "Finansk", "Collabnsk", "Obninsk", "Kertansk"]
     let emails = ["ber@ber.ru", "ber@der.ru", "ber@ger.ru", "ber@ser.ru", "ber@qer.ru"]
     let phones = ["+7903", "+7925", "+7243", "+5431", "+7912"]
     
--- RE_ContactList/Models/Person.swift
+++ RE_ContactList/Models/Person.swift
@@ -13,8 +13,25 @@ struct Person {
     let phone: String
 }
 
-func getPerson() -> [Person] {
-    var contacts: [Person] = []
-    
-    
+extension Person {
+    func getPerson() -> [Person] {
+        var contacts: [Person] = []
+        
+        let names = Contact.shared.names.shuffled()
+        let secondNames = Contact.shared.secondNames.shuffled()
+        let emails = Contact.shared.emails.shuffled()
+        let phones = Contact.shared.phones.shuffled()
+        
+        for index in 0..<names.count {
+            let contact = Person(
+                name: names[index],
+                secondName: secondNames[index],
+                email: emails[index],
+                phone: phones[index]
+            )
+            contacts.append(contact)
+        }
+        return contacts
+    }
 }
+
